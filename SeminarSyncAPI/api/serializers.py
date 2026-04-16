d4b954400f59e85b1718456631aed98d8e1ef2e1
bianananvl <bianca.legaspi10@gmail.com>
2026-04-17 01:30:26 +0800

    Update serializers.py

--- SeminarSyncAPI/api/serializers.py
+++ SeminarSyncAPI/api/serializers.py
@@ -1,69 +1,57 @@
 from rest_framework import serializers
 from .models import College, Department, Faculty, Seminar, AttendanceRecord
 
+
 class CollegeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = College
-        fields = '__all__'
+   class Meta:
+       model = College
+       fields = '__all__'
+       read_only_fields = ['created_at', 'updated_at']
+
 
 class DepartmentSerializer(serializers.ModelSerializer):
-    # Safe check in case a department doesn't have a college (like Admin or HR)
-    college_name = serializers.CharField(source='college.name', read_only=True, default="N/A")
+   college_name = serializers.CharField(source='college.name', read_only=True, default="N/A")
 
-    class Meta:
-        model = Department
-        fields = [
-            'id',
-            'name',
-            'college',
-            'college_name'
-        ]
+
+   class Meta:
+       model = Department
+       fields = ['id', 'name', 'college', 'college_name', 'created_at', 'updated_at']
+       read_only_fields = ['created_at', 'updated_at']
+
 
 class FacultySerializer(serializers.ModelSerializer):
-    department_name = serializers.CharField(source='department.name', read_only=True)
+   department_name = serializers.CharField(source='department.name', read_only=True)
 
-    class Meta:
-        model = Faculty
-        fields = [
-            'id',
-            'employee_number',
-            'last_name',
-            'first_name',
-            'middle_initial',
-            'birthdate',
-            'gender',
-            'department',
-            'department_name',
-            'region_code',
-            'province_code',
-            'city_municipality_code',
-            'barangay_code',
-            'street_address',
-            'email',
-            'phone',
-            'photo',
-            'created_at',
-            'updated_at',
-        ]
+
+   class Meta:
+       model = Faculty
+       fields = [
+           'id', 'employee_number', 'last_name', 'first_name', 'middle_initial',
+           'birthdate', 'gender', 'department', 'department_name',
+           'region_code', 'province_code', 'city_municipality_code',
+           'barangay_code', 'street_address', 'email', 'phone', 'photo',
+           'created_at', 'updated_at',
+       ]
+       read_only_fields = ['created_at', 'updated_at']
+
 
 class SeminarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Seminar
-        fields = '__all__'
+   class Meta:
+       model = Seminar
+       fields = '__all__'
+       read_only_fields = ['created_at', 'updated_at']
+
 
 class AttendanceRecordSerializer(serializers.ModelSerializer):
-    faculty_last_name = serializers.CharField(source='faculty.last_name', read_only=True)
-    faculty_employee_number = serializers.CharField(source='faculty.employee_number', read_only=True)
-    seminar_title = serializers.CharField(source='seminar.title', read_only=True)
+   faculty_last_name = serializers.CharField(source='faculty.last_name', read_only=True)
+   faculty_employee_number = serializers.CharField(source='faculty.employee_number', read_only=True)
+   seminar_title = serializers.CharField(source='seminar.title', read_only=True)
 
-    class Meta:
-        model = AttendanceRecord
-        fields = [
-            'id',
-            'faculty',
-            'faculty_last_name',
-            'faculty_employee_number',
-            'seminar',
-            'seminar_title',
-            'time_logged',
-        ]
+
+   class Meta:
+       model = AttendanceRecord
+       fields = [
+           'id', 'faculty', 'faculty_last_name', 'faculty_employee_number',
+           'seminar', 'seminar_title', 'time_logged',
+       ]
+       read_only_fields = ['time_logged']
