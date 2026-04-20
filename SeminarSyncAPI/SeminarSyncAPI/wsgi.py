acb94a8b418823ec32407c0938ce6e639df2fca3
Juls <juliangavintulao@gmail.com>
2026-04-21 01:09:31 +0800

    Update wsgi.py

--- SeminarSyncAPI/SeminarSyncAPI/wsgi.py
+++ SeminarSyncAPI/SeminarSyncAPI/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SeminarSyncAPI project.
+WSGI config for SeminarSyncAPI2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SeminarSyncAPI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SeminarSyncAPI2.settings')
 
 application = get_wsgi_application()
